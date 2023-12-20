23eb8d249b09899dce0dbd4d2d3049c21507c5da
John Toro <joaltoroc@gmail.com>
2023-12-20 17:42:10 -0500

    Chromatic, Action build

--- src/ui/forms/text-box/text-box.stories.tsx
+++ src/ui/forms/text-box/text-box.stories.tsx
@@ -1,35 +1,35 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from '@storybook/react';
 
-import TextBox from "./text-box.component";
+import TextBox from './text-box.component';
 
 const meta: Meta<typeof TextBox> = {
-  title: "CreativeCode.com.co/UI/Form/TextBox",
+  title: 'CreativeCode.com.co/UI/Form/TextBox',
   component: TextBox,
   argTypes: {
     name: {
-      description: "Unique Name",
-      type: { name: "string", required: true },
+      description: 'Unique Name',
+      type: { name: 'string', required: true }
     },
     label: {
-      description: "Label",
-      type: "string",
+      description: 'Label',
+      type: 'string'
     },
     isError: {
-      description: "TextBox has error",
-      type: "boolean",
+      description: 'TextBox has error',
+      type: 'boolean'
     },
     error: {
-      description: "Error message",
-      type: "string",
+      description: 'Error message',
+      type: 'string'
     },
     disabled: {
-      description: "Disabled",
-      type: "boolean",
-    },
+      description: 'Disabled',
+      type: 'boolean'
+    }
   },
   args: {
-    name: "text-box",
-  },
+    name: 'text-box'
+  }
 };
 
 export default meta;
@@ -37,5 +37,5 @@ export default meta;
 type Story = StoryObj<typeof TextBox>;
 
 export const Primary: Story = {
-  args: {},
+  args: {}
 };
