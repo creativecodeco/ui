c183cda264de20786ff895927a470e81673ebb3f
John Toro <joaltoroc@gmail.com>
2024-01-03 17:37:08 -0500

    Update package, typings.d.ts update

--- typings.d.ts
+++ typings.d.ts
@@ -7,11 +7,3 @@ declare module '*.css' {
   const content: { [className: string]: string };
   export default content;
 }
-
-declare module '*.svg' {
-  const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-  const content: string;
-
-  export { ReactComponent };
-  export default content;
-}
